e505b99c60b4e59d9439fee64672f7902b010e3b
Paren Desai <paren1994@yahoo.com.au>
2018-10-06 17:01:31 -0400

    Login UI

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatInputModule, MatButtonModule } from '@angular/material';
+import { FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
