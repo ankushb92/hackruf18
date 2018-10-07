c9756eeb159f6c6dc2137be40f2f038c46ce00a0
Rui Zhang <r.zhang194@gmail.com>
2018-10-07 05:32:52 -0400

    Help pls

--- frontend/src/app/analytics/analytics.component.ts
+++ frontend/src/app/analytics/analytics.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as c3 from 'c3';
 
 @Component({
   selector: 'app-analytics',
   templateUrl: './analytics.component.html',
   styleUrls: ['./analytics.component.css']
 })
-export class AnalyticsComponent implements OnInit {
+export class AnalyticsComponent implements OnInit, AfterViewInit {
 
   constructor() { }
+    
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+  ngAfterViewInit() {
+    let chart = c3.generate({
+    bindto: '#chart',
+        data: {
+            columns: [
+                ['data1', 30, 200, 100, 400, 150, 250],
+                ['data2', 50, 20, 10, 40, 15, 25]
+            ]
+        }
+    });
+}
 }
