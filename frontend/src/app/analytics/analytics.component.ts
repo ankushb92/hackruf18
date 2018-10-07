c2d0015c196f714c679d68ab2c71a4fdfb3b2cc2
Paren Desai <paren1994@yahoo.com.au>
2018-10-07 11:55:49 -0400

    q

--- frontend/src/app/analytics/analytics.component.ts
+++ frontend/src/app/analytics/analytics.component.ts
@@ -7,15 +7,56 @@ import {WellnessService} from "../wellness.service";
   styleUrls: ['./analytics.component.css']
 })
 export class AnalyticsComponent implements OnInit {
-  data: any[];
+  data1: any[] = [];
+  data2: any[] = [];
   @Input() session: string;
   constructor(public service: WellnessService) {
 
   }
-  
+
+  view: any[] = [700, 300];
+  multi: any[];
+  multi2: any[];
+  // options
+  showXAxis = false;
+  showYAxis = true;
+  gradient = false;
+  showLegend = false;
+  showXAxisLabel = true;
+  xAxisLabel = 'Time';
+  showYAxisLabel = true;
+  yAxisLabel = '$';
+
+  colorScheme = {
+    domain: ['#69f0ae']
+  };
+
+  // line, area
+  autoScale = true;
+
+
 
   ngOnInit() {
-    this.service.get_anal(this.session).subscribe((data)=>{console.log(data)})
+    this.view[0] = window.innerWidth - 30;
+    this.service.get_anal(this.session).subscribe((data)=>{
+      data['transaction'].forEach((e) => {
+        this.data1.push({value: e['p'], name: e['tt']});
+
+      });
+      this.multi = [{name: 'hist', series:this.data1}];
+        console.log(this.data1);
+
+    });
+
+    this.service.get_hold(this.session).subscribe((data)=>{
+      data['holding'].forEach((e) => {
+        this.data2.push({value: e['sum'], name: e['index']});
+
+      });
+      this.multi2 = [{name: 'hist', series:this.data2}];
+      console.log(data);
+
+    });
   }
 
 
--- frontend/src/app/wellness.service.ts
+++ frontend/src/app/wellness.service.ts
@@ -15,4 +15,8 @@ export class WellnessService {
     get_anal(session) {
       return this.http.get(consts.BASE_URL + '/transactions?tnonly', {headers: new HttpHeaders({'session': session})})
     }
+
+    get_hold(session) {
+      return this.http.get(consts.BASE_URL + '/holdings?tnonly', {headers: new HttpHeaders({'session': session})})
+    }
 }
