f156a44b5e083afba75852815d4e67e9dd35c4c2
Paren Desai <paren1994@yahoo.com.au>
2018-10-07 04:46:35 -0400

    temp

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule, MatInputModule, MatButtonModule, MatMenuModule, MatIconModule, MatSnackBarModule, MatTabsModule, MatCardModule, MatDatepickerModule, MatNativeDateModule, MatDividerModule, MatChipsModule, MatExpansionModule} from '@angular/material';
+
 import { FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -39,8 +40,11 @@ import { HomeComponent } from './home/home.component';
     MatCardModule,
     MatDatepickerModule,
     MatNativeDateModule,
+<<<<<<< HEAD
     MatDividerModule,
     MatChipsModule,
+=======
+>>>>>>> temp
     MatExpansionModule
   ],
   providers: [],
