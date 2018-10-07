3562830e79f48b0f93919a491251e8b4ba59a76a
Shyam Mehta <shyammehta@Shyams-MacBook-Pro.local>
2018-10-07 04:45:55 -0400

    Home changes midway

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule, MatInputModule, MatButtonModule, MatMenuModule, MatIconModule, MatSnackBarModule, MatTabsModule, MatCardModule, MatDatepickerModule, MatNativeDateModule } from '@angular/material';
+import { MatToolbarModule, MatInputModule, MatButtonModule, MatMenuModule, MatIconModule, MatSnackBarModule, MatTabsModule, MatCardModule, MatDatepickerModule, MatNativeDateModule, MatDividerModule, MatChipsModule, MatExpansionModule} from '@angular/material';
 import { FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -38,7 +38,10 @@ import { HomeComponent } from './home/home.component';
     MatTabsModule,
     MatCardModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    MatDividerModule,
+    MatChipsModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
